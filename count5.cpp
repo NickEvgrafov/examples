38704b33e9a4702a414654e091124c4a06397511
nevgrafov <nevgrafov@ptsecurity.com>
2018-06-27 16:07:05 +0300

    new count5 solution

--- count5.cpp
+++ count5.cpp
@@ -1,45 +1,72 @@
 #include <iostream>
 
-int getLeftBound5(int val)
-{
-  int k = (val - 5) / 10; 
-  int bound = k * 10 + 5;
-  return bound >= val ? bound : bound + 10;
-}
 
 int getRigthBound5(int val)
 {
   int k = (val - 5) / 10;
   int bound = k * 10 + 5;
-  return bound <= val ? bound : bound - 10;
+  return  (bound > val) ? bound : bound + 10;
 }
 
-int count5(int start, int end)
+const int pow10[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
+
+int getNextCountOf5(int num, int& next)
 {
-  int leftBound5 = getLeftBound5(start);
-  int rightBound5= getRigthBound5(end);
+    int maxPower = 0;
+    bool found = false;
 
-  if (leftBound5 > rightBound5)
-    return 0;
+    for (int base = (num > 0) ? num : -num, power = 0; base != 0; base /= 10, ++power)
+    {
+        if (base % 10 == 5)
+        {
+            found = true;
+            maxPower = power;
+        }
+    }
 
-  return (rightBound5 - leftBound5) / 10 + 1;
+    if (!found)
+    {
+        next = num + 1;
+        return 0;
+    }
+
+    if (num > 0)
+        next = num / pow10[maxPower] * pow10[maxPower] + pow10[maxPower];
+    else
+        next = num / pow10[maxPower] * pow10[maxPower] + 1;
+        
+    return next - num;
 }
-
 
 int dontGiveMeFive(int start, int end)
 {
-  if (start > end)
-    return 0;
-  return (end - start + 1) - count5(start, end);
+    if (start > end)
+       return 0;
+
+    int num = start;
+    int totalCount5 = 0;
+
+    while (num <= end)
+    {
+        int next = 0;
+        int count5 = getNextCountOf5(num, next);
+        totalCount5 += count5;
+        num = next;
+    }
+    
+    return (end - start + 1) - totalCount5;
 }
 
 int main()
-{
+{    
     int start = 1;
-    int end = 7;
-
-    int ret = dontGiveMeFive(start, end);
-    std::cout << start << "-" << end << ": " << ret << std::endl;
+    int end = 90;
+    int i = 49;
+//for (int i = end - 1; i >= start; --i)
+    //{
+    
+        std::cout << "[" << i << ";" << end <<" ]: " << dontGiveMeFive(i, end)  << std::endl;
+        //}
 
     return 0;
 }
